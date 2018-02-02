43539d43da7fe93e1fe1ff0a79902da8a0c8de23
Ning LIU <ning.liu.cn@outlook.com>
2018-02-02 19:59:59 +0100

    Add files via upload

---
+++ random-quote-machine/js/index.js
@@ -0,0 +1,67 @@
+
+function inIframe () { try { return window.self !== window.top; } catch (e) { return true; } }
+
+var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
+var currentQuote = "";
+var currentAuthor = "";
+function openURL(url){
+  window.open(url, "Twitter",'width=550, height=400, toolbar=1, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0', false);
+}
+function getQuote() {
+  $.ajax({
+    headers: {
+      "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
+    success: function(r) {
+      if (typeof r === 'string') {
+       r = JSON.parse(r); 
+      }
+      currentQuote = r.quote;
+      currentAuthor = r.author;
+      if(inIframe())
+      {
+        $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
+      }
+      $(".quote-text").animate({
+          opacity: 0
+        }, 500,
+        function() {
+          $(this).animate({
+            opacity: 1
+          }, 500);
+          $('#text').text(r.quote);
+        });
+
+      $(".quote-author").animate({
+          opacity: 0
+        }, 500,
+        function() {
+          $(this).animate({
+            opacity: 1
+          }, 500);
+          $('#author').html(r.author);
+        });
+
+      var color = Math.floor(Math.random() * colors.length);
+      $("body").animate({
+        backgroundColor: colors[color],
+        color: colors[color]
+      }, 1000);
+      $(".button").animate({
+        backgroundColor: colors[color]
+      }, 1000);
+    }
+  });
+}
+$(document).ready(function() {
+  //getQuote();
+  $('#new-quote').on('click', getQuote);
+  $('#tweet-quote').on('click', function() {
+    if(!inIframe()) {
+      openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
+    }
+  });
+});
